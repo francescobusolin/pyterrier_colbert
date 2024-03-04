9bd9b4bd152e45c3be82d86fa682e71c43b4465e
francesco <francesco.busolin@libero.it>
2024-03-04 10:24:04 +0000

    stopwords add

--- pyterrier_colbert/preprocessing.py
+++ pyterrier_colbert/preprocessing.py
@@ -1,21 +1,10 @@
 class CorpusIteratorPreprocessor:
-    _default_stopwords = set([
-        'the', 'a', 'an', 'in', 'on', 'at', 'as', 'of', 'by',
-        " ' ", "''", "``", "(", ")", "[", "]", "{", "}", ",", ".",
-        ":", ";", "!", "?", "&", "-", "--", "...", "''", "``", "..."
-                                                               '"', '""', "''", "``", "(", ")", "[", "]", "{", "}", ",",
-        ".", ":", ";", "!", "?", "&", "-", "--", "..."
-                                                 '<', '>', '=', '+', '*', '@', '#', '$', '%', '^', '_', '`', '~', '\\',
-        '/', '|',
-        '.', ',', '!', '?', ';', ':', '-', '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '@', '#', '$', '%',
-        '^', '&', '*', '+', '=', '_', '`', '~'
-    ])
 
     def __init__(self, corpus_iterator, tokenizer, preprocessor=None):
         self.corpus_iterator = corpus_iterator
         self.tokenizer = tokenizer
-        if preprocessor is None:
-            self.preprocessor = self._default_preprocessor
+        if (preprocessor is None) or (preprocessor == 'default'):
+            self.preprocessor = TokenRemover(['../stopwords/stopwords-en.txt'], self.tokenizer)
         else:
             self.preprocessor = preprocessor
 
@@ -23,13 +12,19 @@ class CorpusIteratorPreprocessor:
         for document in self.corpus_iterator:
             yield self.preprocessor(document)
 
-    def _default_preprocessor(self, document):
-        # document has form {'text': 'some text', 'docno': number}
+
+class TokenRemover:
+    def __init__(self, stopwords_files, tokenizer):
+        self.tokenizer = tokenizer
+        self.stopwords = set()
+        for file in stopwords_files:
+            with open(file, 'r') as f:
+                self.stopwords = self.stopwords | set(f.read().splitlines())
+
+    def __call__(self, document):
         doc_text = document['text']
         doc_tokenized = self.tokenizer.tokenize(doc_text)
-        doc_cleaned = [tok for tok in doc_tokenized if tok not in self._default_stopwords]
+        doc_cleaned = [tok for tok in doc_tokenized if tok not in self.stopwords]
         cleaned_text = self.tokenizer.convert_tokens_to_string(doc_cleaned)
 
         return {'text': cleaned_text, 'docno': document['docno']}
-
-
